1f1064f7eb0faa5abedf36be65bd991c6b31a79d
dreamcareer7 <dreamcareer070@gmail.com>
2020-12-23 12:52:20 +0530

    Init React app

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+#Human pixel link
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Human Pixel
+    </div>
+  );
+}
+
+export default App;
