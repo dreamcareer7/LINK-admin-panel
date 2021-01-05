e05047ff22a61991fa749cc2e36875bc15f92769
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-05 11:12:47 +0530

    Fixes in UI

--- src/App.js
+++ src/App.js
@@ -7,34 +7,15 @@ import VerificationPage from './components/authentication/verification-page/Veri
 import ForgotPasswordPage from './components/authentication/forgotpassword-page/ForgotPasswordPage';
 import SetNewPassword from './components/authentication/set-new-password/SetNewPassword';
 import Dashboard from './components/dashboard/Dashboard';
-// import QuoteBank from './components/dashboard/quoteBank/QuoteBank';
 import AddQuote from './components/dashboard/quoteBank/add-edit-quote/AddQuote';
-// import Settings from './components/dashboard/settings/Settings';
-// import AddQuote from './components/dashboard/quoteBank/add-quote/AddQuote';
 import ErrorMessages from './components/dashboard/settings/error-messages/ErrorMessages';
 import Integrations from './components/dashboard/settings/integrations/Integrations';
 import ManageAdmins from './components/dashboard/settings/manage-admins/ManageAdmins';
-// import EditQuote from './components/dashboard/quoteBank/add-edit-quote/EditQuote';
 import Layout from './components/commonComponents/layout/Layout';
 import Settings from './components/dashboard/settings/Settings';
 import Subscribers from './components/dashboard/subscribers/Subscribers';
 import QuoteBank from './components/dashboard/quoteBank/QuoteBank';
 import { getAuthTokenLocalStorage } from './helpers/LocalStorageHelper';
-// import { getAuthTokenLocalStorage } from './helpers/LocalStorageHelper';
-// import { getLogedInUser } from './redux/actions/authActions/AuthActions';
-
-// const PrivateRoute = ({ component, ...options }) => {
-//   // const dispatch = useDispatch();
-//   // useEffect(() => {
-//   //   dispatch(getLogedInUser());
-//   // }, []);
-//   const finalComponent =
-//     localStorage.getItem('userToken') !== null && localStorage.getItem('userToken').length !== 0
-//       ? component
-//       : LoginPage;
-
-//   return <Route {...options} component={finalComponent} />;
-// };
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const isLoggedIn = getAuthTokenLocalStorage();
@@ -48,14 +29,6 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
         isLoggedIn !== null ? (
           <Component {...props} />
         ) : (
-          // component
-          // <Redirect
-          //   to={{
-          //     pathname: '/dashboard',
-          //     // eslint-disable-next-line react/prop-types
-          //     state: { from: props.location },
-          //   }}
-          // />
           <Redirect
             to={{
               pathname: '/login',
@@ -90,7 +63,11 @@ function App() {
               <PrivateRoute exact path="/error-message" component={ErrorMessages} />
               <PrivateRoute exact path="/integrations" component={Integrations} />
               <PrivateRoute exact path="/manage-admin" component={ManageAdmins} />
-              <PrivateRoute exact path="/verificationPage" render={props => <VerificationPage {...props} />} />
+              <PrivateRoute
+                exact
+                path="/verificationPage"
+                render={props => <VerificationPage {...props} />}
+              />
               <PrivateRoute exact path="/quoteBank" render={props => <QuoteBank {...props} />} />
               <PrivateRoute exact path="/quote" render={props => <AddQuote {...props} />} />
               <PrivateRoute exact path="/quote/:id" render={props => <AddQuote {...props} />} />
