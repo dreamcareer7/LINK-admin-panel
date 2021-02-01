3f91b09ba13b02c3dd32dc70850559e5d89e46c6
dreamcareer7 <dreamcareer070@gmail.com>
2021-02-01 20:21:52 +0530

    Added redux persist

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {store} from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <React.StrictMode>
-            <App/>
-        </React.StrictMode>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,22 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import {composeWithDevTools} from "redux-devtools-extension";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers/RootReducer';
 
-export const store = createStore(
-    rootReducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
-    composeWithDevTools(applyMiddleware(
-        thunkMiddleware,
-        )
-    ));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(thunkMiddleware))
+);
+
+export const persistor = persistStore(store);
+
 export default store;
