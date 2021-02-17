038205810e2cb6eace9397135aef22ad705a3213
dreamcareer7 <dreamcareer070@gmail.com>
2021-02-17 18:15:14 +0530

    Auth Text Input Input placeholder focus issue done

--- src/components/authentication/common/text-input/AuthTextInput.js
+++ src/components/authentication/common/text-input/AuthTextInput.js
@@ -2,27 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function AuthTextInput(props) {
-  const { onChange, value, placeholder, type, src } = props;
-  return (
-    <div className="form--detail-container">
-      <div className="detail-icon">
-        <img alt="" src={src} />
-      </div>
-      <input className="" type={type} onChange={onChange} value={value} placeholder={placeholder} />
-    </div>
-  );
+    const {onChange, value, placeholder, type, src} = props;
+    return (
+            <div className="form--detail-container">
+                <div className="detail-icon">
+                    <img alt="" src={src}/>
+                </div>
+                <input className="" type={type} onChange={onChange} value={value} placeholder={placeholder}
+                       onFocus={(e) => {e.target.placeholder = ""}}
+                       onBlur={(e) => {e.target.placeholder = placeholder}}/>
+            </div>
+    );
 }
 
 AuthTextInput.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  type: PropTypes.string,
-  src: PropTypes.element,
-  value: PropTypes.string.isRequired,
-  placeholder: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    type: PropTypes.string,
+    src: PropTypes.element,
+    value: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired,
 };
 AuthTextInput.defaultProps = {
-  type: 'text',
-  src: '',
+    type: 'text',
+    src: '',
 };
 
 export default AuthTextInput;
