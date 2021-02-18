abb07a30fdebb6f5ec3582ff8d28d64b612efc0a
dreamcareer7 <dreamcareer070@gmail.com>
2021-02-18 18:40:42 +0530

    Sidebar logo changes

--- src/components/commonComponents/sidebar/SideBar.js
+++ src/components/commonComponents/sidebar/SideBar.js
@@ -12,7 +12,7 @@ function SideBar() {
     <div>
       <div className="">
         <NavLink className="dashboard-logo" to="/dashboard" replace>
-          <img alt="Linkfluencer" src={linkfluencer} style={{ width: '70%' }} />
+          <img alt="Linkfluencer" src={linkfluencer} />
         </NavLink>
       </div>
       <div className="menu-bar">
